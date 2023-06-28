a894fcac7c1afa4d441a0f923bfd451afe629fb4
Jesus Hernandez <jesus.clemente@elektra.com.mx>
2023-06-28 17:21:24 -0600

    WelcomeScreen

--- app/src/main/java/com/github/jesushzc/calorytracker/MainActivity.kt
+++ app/src/main/java/com/github/jesushzc/calorytracker/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.github.jesushzc.calorytracker.ui.theme.CaloryTrackerTheme
+import com.github.jesushzc.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
-
+                WelcomeScreen()
             }
         }
     }
