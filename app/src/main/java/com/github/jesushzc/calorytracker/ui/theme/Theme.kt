32b4b1d78be80baea2057c80654bf2e6f98909bb
Jesus Hernandez <jesus.clemente@elektra.com.mx>
2023-06-28 14:51:23 -0600

    Migrate to Kotlin DSL

--- app/src/main/java/com/github/jesushzc/calorytracker/ui/theme/Theme.kt
+++ app/src/main/java/com/github/jesushzc/calorytracker/ui/theme/Theme.kt
@@ -1,16 +1,15 @@
 package com.github.jesushzc.calorytracker.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColorScheme(
+private val DarkColorPalette = darkColors(
     primary = BrightGreen,
-    inversePrimary = DarkGreen,
+    primaryVariant = DarkGreen,
     secondary = Orange,
     background = MediumGray,
     onBackground = TextWhite,
@@ -20,9 +19,9 @@ private val DarkColorPalette = darkColorScheme(
     onSecondary = Color.White,
 )
 
-private val LightColorPalette = lightColorScheme(
+private val LightColorPalette = lightColors(
     primary = BrightGreen,
-    inversePrimary = DarkGreen,
+    primaryVariant = DarkGreen,
     secondary = Orange,
     background = Color.White,
     onBackground = DarkGray,
@@ -33,16 +32,16 @@ private val LightColorPalette = lightColorScheme(
 )
 
 @Composable
-fun CaloryTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CaloryTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
     MaterialTheme(
+        colors = colors,
         typography = Typography,
         shapes = Shapes,
-        content = content,
-        colorScheme = colors
+        content = content
     )
 }
---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,8 @@
+object ProjectConfig {
+    const val appId = "com.github.jesushzc.calorytracker"
+    const val compileSdk = 33
+    const val minSdk = 27
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
