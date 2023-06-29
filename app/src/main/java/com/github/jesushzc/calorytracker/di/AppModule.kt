f79aa8ebf5d41cb320a3e2062e4af5e907373be3
Jesus Hernandez <jesus.clemente@elektra.com.mx>
2023-06-28 23:44:53 -0600

    Preferences setup

---
+++ app/src/main/java/com/github/jesushzc/calorytracker/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.github.jesushzc.calorytracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.github.jesushzc.core.data.DefaultPreferences
+import com.github.jesushzc.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+
+}
